9dae23c55247751fd5d8fb829f35391cc294c7e3
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2023-10-04 15:06:51 -0700

    rename

---
+++ Embeded_text/Champions.hpp
@@ -0,0 +1,6 @@
+#ifndef CHAMPIONS_CSV_HPP
+#define CHAMPIONS_CSV_HPP
+
+extern const char* CHAMPIONS_CSV;
+
+#endif
---
+++ Embeded_text/Traits.hpp
@@ -0,0 +1,6 @@
+#ifndef TRAITS_CSV_HPP
+#define TRAITS_CSV_HPP
+
+extern const char* TRAITS_CSV;
+
+#endif

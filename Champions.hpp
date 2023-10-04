8f2d1f81200afd834824eae7aea56444612cd367
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2023-10-04 14:55:58 -0700

    removed files

--- Champions.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef CHAMPIONS_CSV_HPP
-#define CHAMPIONS_CSV_HPP
-
-extern const char* CHAMPIONS_CSV;
-
-#endif
--- Prompts.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PROMPTS_HPP
-#define PTOMPTS_HPP
-
-extern const char* main_options;
-extern const char* separator;
-extern const char* goodbye;
-#endif 

d7df0972419edeb39be4bc1d239086b99d8655fd
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2024-03-30 12:46:16 -0700

    fast_vector_assignment

--- Fast_Vector.cpp
+++ Fast_Vector.cpp
@@ -8,7 +8,10 @@
 // constructor
 Fast_Vector::Fast_Vector(){
     vector = 0;
-    element_count = 0;
+}
+
+Fast_Vector::Fast_Vector(unsigned long long fast_vec_num){
+    vector = fast_vec_num;
 }
 
 // nothing to deconstruct
@@ -19,18 +22,33 @@ Fast_Vector::~Fast_Vector(){
 // pushes back an integer
 void Fast_Vector::push_back(int x){
     vector |= 0x8000000000000000 >> x;
-    element_count++;
 }
 
 // pops back the highest integer NOT NECESSARILY THE LAST ELEMENT
 int Fast_Vector::pop_back(){
     int position = __builtin_ctzll(vector);
     vector ^= (1ULL << position);
-    element_count--;
     return 63 - position;
 }
 
 // peeks at the highest integer NOT NECESSARILY THE LAST ELEMENT
 int Fast_Vector::back(){
     return 63 - __builtin_ctzll(vector);
+}
+
+// peeks at the highest integer NOT NECESSARILY THE LAST ELEMENT
+int Fast_Vector::size(){
+    return __builtin_popcountll(vector);
+}
+
+// returns the Fast_Vector as a number
+unsigned long long Fast_Vector::num(){
+    return vector;
+}
+
+// custom assignment
+Fast_Vector& Fast_Vector::operator=(const std::vector<int>& v1){
+    for(int i = 0; i < v1.size(); i++){
+        push_back(v1[i]);
+    }
 }

96274e9e4af0a93eb9760b2eadb84c5c4ab5885f
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2024-03-30 02:47:16 -0700

    debugging

--- Fast_Vector.cpp
+++ Fast_Vector.cpp
@@ -17,17 +17,19 @@ Fast_Vector::~Fast_Vector(){
 
 // pushes back an integer
 void Fast_Vector::push_back(int x){
-    vector |= 1ULL << x;
+    vector |= 0x8000000000000000 >> x;
+    element_count++;
 }
 
 // pops back the highest integer NOT NECESSARILY THE LAST ELEMENT
 int Fast_Vector::pop_back(){
     int position = __builtin_ctzll(vector);
     vector ^= (1ULL << position);
-    return position;
+    element_count--;
+    return 63 - position;
 }
 
 // peeks at the highest integer NOT NECESSARILY THE LAST ELEMENT
 int Fast_Vector::back(){
-    return __builtin_ctzll(vector);
+    return 63 - __builtin_ctzll(vector);
 }

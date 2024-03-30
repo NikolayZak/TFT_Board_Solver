d938c8d5403fc56e9a03ba5cc022d2885b801fa1
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2024-03-29 21:13:32 -0700

    fast vector

---
+++ BDT.cpp
@@ -0,0 +1,30 @@
+#include "BDT.hpp"
+/*          Time Complexity
+    let t be the number of traits in the set
+    let t` be the number of traits any given champ has
+    let t`` be the number of tiers any given trait has
+    let c be the number of champions in the set
+*/
+
+// constructor
+Board_Vector::Board_Vector(){
+    Vector = 0;
+}
+
+// nothing to deconstruct
+Board_Vector::Board_Vector(){
+
+}
+
+
+void push_back(int x){
+    
+}
+
+int pop_back(){
+
+}
+
+int back(){
+
+}

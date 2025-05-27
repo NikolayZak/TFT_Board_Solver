c8a0861bbbf90654795da7e100ac9da691a04423
NikolayZ <nikolayzakirov31@gmail.com>
2025-05-26 21:49:23 -0700

    StaticVariables
    for speed ofc

---
+++ Legacy/StaticVariables.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#define MAX_CHAMPION_TRAITS 5 // Maximum number of traits a champion can have
+#define MAX_TRAIT_TIERS 15 // Maximum number of tiers a trait can have
+#define MAX_CHAMPIONS 64 // Maximum number of champions in a set

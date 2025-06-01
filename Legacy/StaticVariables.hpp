d3eca078336f5b22340a5b6e1156ead6ab42cf33
NikolayZ <nikolayzakirov31@gmail.com>
2025-05-31 22:34:28 -0700

    Local Database Path

--- Legacy/StaticVariables.hpp
+++ Legacy/StaticVariables.hpp
@@ -3,3 +3,4 @@
 #define MAX_TRAIT_TIERS 15 // Maximum number of tiers a trait can have
 #define MAX_CHAMPIONS 64 // Maximum number of champions in a set
 #define MAX_PLAYER_LEVEL 10 // Maximum player level in the game
+#define LOCAL_DB_PATH "database.db" // Path to the local database file

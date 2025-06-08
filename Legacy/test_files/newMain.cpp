35ff8b9fde7192d688d78026fbb137b4119113c9
NikolayZ <nikolayzakirov31@gmail.com>
2025-06-07 22:04:27 -0700

    test files configs

--- Legacy/test_files/newMain.cpp
+++ Legacy/test_files/newMain.cpp
@@ -7,8 +7,8 @@ int main() {
     LocalDB db("database.db");
     SetData set_data = db.allocSet(11);
     //set_data.restrictSet(1, {}); // Restrict the set data for player level 1
-    Solver solver(set_data, 50); // Create a solver with heap size of 10
-    vector<BoardResult> answer = solver.Solve(1);
+    Solver solver(set_data, 100); // Create a solver with heap size of 10
+    vector<BoardResult> answer = solver.Solve(8);
     cout << "Optimal Boards:\n";
     for (const auto& board_result : answer) {
         board_result.Print();

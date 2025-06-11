0e4e54d2d752d1844c78a2b6ade6a8b1fd142ea0
NikolayZ <nikolayzakirov31@gmail.com>
2025-06-10 21:55:27 -0700

    test files

--- Legacy/test_files/newMain.cpp
+++ Legacy/test_files/newMain.cpp
@@ -1,14 +1,23 @@
 #include "../LocalDB.hpp"
 #include "../Solver.hpp"
 
+
+
+
 // compile test with: g++ ../FastVector.cpp ../Common.cpp ../TopScoringBoards.cpp ../Serialisation.cpp ../LocalDB.cpp ../Board.cpp ../Solver.cpp newMain.cpp -lsqlite3 -o test
 int main() {
     // Initialize the local database
     LocalDB db("database.db");
     SetData set_data = db.allocSet(11);
-    //set_data.restrictSet(1, {}); // Restrict the set data for player level 1
-    Solver solver(set_data, 100); // Create a solver with heap size of 10
-    vector<BoardResult> answer = solver.Solve(8);
+    set_data.restrictSet(10, {}); // Restrict the set data (optional)
+    Solver solver(set_data, 100); // Create a solver with heap size of n
+    vector<BoardResult> answer = solver.Solve(8); // compute the target size
+
+    // send result to crow 
+
+
+
+    // testing
     cout << "Optimal Boards:\n";
     for (const auto& board_result : answer) {
         board_result.Print();

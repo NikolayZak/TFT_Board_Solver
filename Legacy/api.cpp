99c80d03ffe7d1913f65511336fdee3a3d8a9314
NikolayZ <nikolayzakirov31@gmail.com>
2025-06-13 18:40:09 -0700

    api file

--- Legacy/api.cpp
+++ Legacy/api.cpp
@@ -1,2 +1,23 @@
-#include "Solver.hpp"
+#include "RequestHandler.hpp"
 
+int main() {
+    crow::SimpleApp app;
+    RequestHandler handler;
+
+    CROW_ROUTE(app, "/api/compute").methods("POST"_method)
+    ([&handler](const crow::request& req){
+        return handler.handle_compute(req);
+    });
+
+    CROW_ROUTE(app, "/api/job-status").methods("GET"_method)
+    ([&handler](const crow::request& req){
+        return handler.handle_status(req);
+    });
+
+    CROW_ROUTE(app, "/api/static").methods("GET"_method)
+    ([&handler](const crow::request& req){
+        return handler.handle_static(req);
+    });
+
+    app.port(18080).multithreaded().run();
+}
